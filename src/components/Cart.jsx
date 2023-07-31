882b191f8237e913a864c975d4310ec266d14853
Federico Massara <federico.massara2@gmail.com>
2023-07-31 17:23:12 +0200

    add cart logic

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import cart from '../images/icon-cart.svg';
+import deleteIcon from '../images/icon-delete.svg'
+import imageProduct from '../images/image-product-1-thumbnail.jpg'
 
 
 const Cart = ({ itemQty, setItemQty}) => {
@@ -20,7 +22,17 @@ const Cart = ({ itemQty, setItemQty}) => {
       </a> 
         <p id="item-qty">{itemQty}</p>
       {seeCart && <div className="dropdown-menu">
-        Your cart is empty.
+        <h5 className="cart-heading">Cart</h5>
+        {itemQty > 0 && <div className="cart-wrapper">
+          <div className="cart-item">
+          <img src={imageProduct} alt="" className="cart-item__picture" />
+          <h5 className="cart-item__title">Fall Limited Edition Sneakers $125.00 x {itemQty} <span className='bold'>${125*itemQty}.00</span></h5>
+          <img src={deleteIcon} alt="" className="cart-item__delete-icon" />
+          </div>
+        <button className="btn" id='btn-checkout'>Checkout</button>
+        </div>}
+        
+        {itemQty === 0 && "Your cart is empty."}
       </div>}
       
     </div>
