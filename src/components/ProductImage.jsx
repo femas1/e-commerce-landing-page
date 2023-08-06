1aec4aa728d4e1bac04851e68aa8f06a71550086
femas1 <federico.massara2@gmail.com>
2023-08-06 16:47:58 +0200

    add lighbox

--- src/components/ProductImage.jsx
+++ src/components/ProductImage.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { useState } from 'react';
+import FsLightbox from "fslightbox-react";
+
 import imageOne from '../images/image-product-1.jpg'
 import thumbnailOne from '../images/image-product-1-thumbnail.jpg'
 import imageTwo from '../images/image-product-2.jpg'
@@ -9,11 +12,28 @@ import imageFour from '../images/image-product-4.jpg'
 import thumbnailFour from '../images/image-product-4-thumbnail.jpg'
 
 const ProductImage = () => {
+
+  const [toggler, setToggler] = useState(false);
+
   return (
     <div className='product-image'>
       <div className="images">
         <div className="main">
-            <img src={imageOne} alt="" className="main-img" />
+            <img 
+                  src={imageOne} 
+                  alt="" 
+                  className="main-img"
+                  onClick={() => setToggler(!toggler)}
+           />
+           <FsLightbox
+				toggler={toggler}
+				sources={[
+            imageOne,
+            imageTwo,
+            imageThree,
+            imageFour
+				]}
+			/>
         </div>
         <div className="thumbnails">
             <img src={thumbnailOne} alt="" className="thumbnail-1" />
@@ -21,8 +41,6 @@ const ProductImage = () => {
             <img src={thumbnailThree} alt="" className="thumbnail-3" />
             <img src={thumbnailFour} alt="" className="thumbnail-4" />
         </div>
-        
-       
       </div>
     </div>
   )
