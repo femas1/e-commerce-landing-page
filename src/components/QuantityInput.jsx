7918ad4f6ceb8ffab27ed1cd8606cfbc0fb79df9
femas1 <federico.massara2@gmail.com>
2023-08-08 17:43:26 +0200

    implement increment and decrement function for inputValue

--- src/components/QuantityInput.jsx
+++ src/components/QuantityInput.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
 
 const QuantityInput = ({ inputValue, setInputValue}) => {
+
+  const increaseValue = () => {
+    setInputValue(Number(inputValue + 1))
+  }
+
+  const decreaseValue = () => {
+    if(inputValue > 0){
+    setInputValue(Number(inputValue -1, 0))
+    }else return
+  }
+
+
+
   return (
     <>
+        <button 
+            className="btn" 
+            id="decrease-btn"
+            onClick={decreaseValue}
+        >-</button>
         <input 
             type="number" 
             min={0} 
             value={inputValue}
             onChange={(e) => {setInputValue(Number(e.target.value))}}
          /> 
+        <button 
+              className="btn" 
+              id="increase-btn"
+              onClick={increaseValue}
+        >+</button>
     </>
   )
 }
